81e38f5c16d3c26e197e1916dd72bddb0e38b8e7
lehenric <hnrchl@gmail.com>
2019-09-25 21:50:12 +0200

    general rework

--- setup.sh
+++ setup.sh
@@ -2,10 +2,33 @@
 
 res_x=1280
 res_y=800
-cvt_out=$(cvt $res_x $res_y | sed -nE 's/Modeline (.*)/\1/p')
-res_whole=$(echo $cvt_out | sed -nE 's/.*"(.*)".*/\1/p')
-xrandr --newmode $cvt_out
+export VIRTUAL_DISP=VIRTUAL1
 
-xrandr --addmode VIRTUAL1 "$res_whole"
-xrandr --output VIRTUAL1 --mode "$res_whole" --left-of eDP1
-x11vnc -clip xinerama0 --forever
+set -euo pipefail
+
+cleanup() {
+    echo "Exiting"
+    set +eu #disable errors when killing children (ps prints itself)
+    # kill all children heh
+    for child in $(ps -o pid= --ppid $$)
+    do
+        kill $child
+    done
+    set -eu # enable it again
+    xrandr --output $VIRTUAL_DISP --off
+    xrandr --delmode $VIRTUAL_DISP "$MODE"
+    xrandr --rmmode "$MODE"
+}
+trap "cleanup" TERM INT HUP
+
+export CVT=$(cvt $res_x $res_y | sed -nE 's/Modeline (.*)/\1/p')
+export MODE=$(echo $CVT | sed -nE 's/.*"(.*)".*/\1/p')
+
+xrandr --newmode `xargs printf '%s ' <<< "$CVT"` # use echo to bypass adding quotes
+xrandr --addmode $VIRTUAL_DISP "$MODE"
+xrandr --output $VIRTUAL_DISP --mode "$MODE" --left-of eDP1
+
+echo "Prepared xrandr for x11vnc, press ENTER to start x11vnc"
+read
+x11vnc -clip xinerama0 --forever &
+wait
